f6c76050030e7361e70bde20b9c7b7896b213e33
jeanport <pierreanatc522@gmail.com>
2025-07-07 16:40:34 -0300

    Projeto inicial

---
+++ chamadasproj/src/main/java/com/malcon/chamadasproj/ChamadasprojApplication.java
@@ -0,0 +1,13 @@
+package com.malcon.chamadasproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadasprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadasprojApplication.class, args);
+	}
+
+}
---
+++ chamadasproj/src/test/java/com/malcon/chamadasproj/ChamadasprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcon.chamadasproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadasprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
